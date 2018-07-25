b5b33b7ac54f7aeac2108056442b9cdda8394ed9
James Wilson <james.wilson@gforces.co.uk>
2018-07-25 19:35:58 +0100

    line endings

--- helloworld/main.go
+++ helloworld/main.go
@@ -1,7 +1,8 @@
 package main //Main indicates its an executable package.
 
-import "fmt"
+import "fmt" //Give my package aka main access to all the functionality in fmt, fmt stands for format
 
 func main() {
 	fmt.Println("Hi there")
+	
 }
